d6e8b3effc935e73843bc07aa0c8fb249df38904
Dhaval Thakkar <Dhaval93@Dhavals-MacBook-Air.local>
2017-11-22 16:50:14 +0530

    updated code with comments

--- ensemble_emotion_simulation.py
+++ ensemble_emotion_simulation.py
@@ -1,8 +1,7 @@
-"""ensemble_emotion_simulation.py: purpose of this script is to predict the emotion from the speech using traind
+"""ensemble_emotion_simulation.py: purpose of this script is to predict the emotion from the speech using trained
 ensemble model based Toronto emotion speech dataset """
 
 __author__ = "Dhaval Thakkar"
-__purpose__ = "Part of Omnisys Solution internship selection process"
 
 import glob
 import librosa
@@ -10,6 +9,13 @@ import librosa.display
 import numpy as np
 import _pickle as pickle
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+
+# Method to extract features from speech using librosa
 
 
 def extract_feature(file_name):
@@ -24,10 +30,14 @@ def extract_feature(file_name):
     return mfccs, chroma, mel, contrast, tonnetz
 
 
+# List to store file names
 target_files = []
+
+# Method to extract label name and extract features from audio file
 
 
 def parse_audio_files(path):
+    labels = []
     features = np.empty((0, 193))
     for fn in glob.glob(path):
         try:
@@ -37,17 +47,49 @@ def parse_audio_files(path):
             continue
         ext_features = np.hstack([mfccs, chroma, mel, contrast, tonnetz])
         features = np.vstack([features, ext_features])
+        labels = np.append(labels, fn.split("_")[3].split(".")[0])
         target_files.append(fn)
-    return np.array(features)
+    return np.array(features), np.array(labels)
 
 
-ts_features = parse_audio_files('./test_sounds/*.wav')
-tr_features = np.array(ts_features, dtype=pd.Series)
+# Get labels and features of audion file of specified path
+ts_features, ts_labels = parse_audio_files('./test_sounds/*.wav')
 
+# Convert features and labels to the pandas Series data type
+ts_features = np.array(ts_features, dtype=pd.Series)
+ts_labels = np.array(ts_labels, dtype=pd.Series)
+
+# Trained Model file name
 filename = 'Ensemble_Model_protocol2.sav'
+
+# Load saved model from file
 model = pickle.load(open(filename, 'rb'))
 
+# Predict the emotion class
 prediction = model.predict(ts_features)
 
+# True labels
+test_true = ts_labels
+
+# List to store prediction
+test_predicted = []
+
+# Iterate over model prediction and store it into list
 for i, val in enumerate(prediction):
-    print("Input File: ", target_files[i], "|", " Predicted Emotion Is:", val)
+    test_predicted.append(val)
+
+
+# Accuracy score of model
+print('Accuracy Score:', accuracy_score(test_true, test_predicted))
+
+# Number of corrected prediction
+print('Number of correct prediction:', accuracy_score(test_true, test_predicted, normalize=False), 'out of', len(ts_labels))
+
+# Plotting confusion matrix
+matrix = confusion_matrix(test_true, test_predicted)
+classes = list(set(ts_labels))
+classes.sort()
+df = pd.DataFrame(matrix, columns=classes, index=classes)
+plt.figure()
+sn.heatmap(df, annot=True)
+plt.show()
--- ensemble_emotion_training.py
+++ ensemble_emotion_training.py
@@ -2,7 +2,6 @@
 dataset to predict the emotion from speech """
 
 __author__ = "Dhaval Thakkar"
-__purpose__ = "Part of Omnisys Solution internship selection process"
 
 
 import glob
@@ -15,6 +14,8 @@ from sklearn import model_selection
 from sklearn.ensemble import VotingClassifier
 import pandas as pd
 
+# Method to extract features from speech using librosa
+
 
 def extract_feature(file_name):
     X, sample_rate = librosa.load(file_name)
@@ -26,6 +27,8 @@ def extract_feature(file_name):
     tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
                                               sr=sample_rate).T, axis=0)
     return mfccs, chroma, mel, contrast, tonnetz
+
+# Method to extract label name and extract features from audio file
 
 
 def parse_audio_files(path):
@@ -44,30 +47,39 @@ def parse_audio_files(path):
     return np.array(features), np.array(labels)
 
 
+# Get labels and features of audion file of specified path
 tr_features, tr_labels = parse_audio_files('./training_sounds/*.wav')
 
 print(tr_labels)
 
+
+# Convert features and labels to the pandas Series data type
 tr_features = np.array(tr_features, dtype=pd.Series)
 tr_labels = np.array(tr_labels, dtype=pd.Series)
 
+# Model1:- support vector classifier using linear kernel
 model1 = svm.SVC(kernel='linear', C=1000, gamma='auto')
 model1.fit(X=tr_features.astype(int), y=tr_labels.astype(str))
 
+# Model2:- support vector classifier using RBF kernel
 model2 = svm.SVC(kernel='rbf', C=1000, gamma='auto')
 model2.fit(X=tr_features.astype(int), y=tr_labels.astype(str))
 
 seed = 7
 kfold = model_selection.KFold(n_splits=10, random_state=seed)
-estimators = [('linear', model1), ('rbf', model2)]
-ensemble = VotingClassifier(estimators)
 
+# Ensemble the linear and RBF kernel model
+estimators = [('linear', model1), ('rbf', model2)]
+# Voting Classifier
+ensemble = VotingClassifier(estimators)
 results = model_selection.cross_val_score(ensemble, tr_features.astype(int), tr_labels.astype(str), cv=kfold)
 ensemble.fit(X=tr_features.astype(int), y=tr_labels.astype(str))
 
+# File name to store the trained model
 filename = 'Ensemble_Model_protocol2.sav'
 
+# Store the trained model
 pickle.dump(ensemble, open(filename, 'wb'), protocol=2)
 
 print('Model Saved..')
-print('Score:', ensemble.score(X=tr_features.astype(int), y=tr_labels.astype(str)))
+
